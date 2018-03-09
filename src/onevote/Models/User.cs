82f0cd20340a8b50eee04dfb014cb1d2afbdb990
Gregory Goh (Liang) <gohliang@gmail.com>
2018-03-09 02:42:52 -0500

    Add Models and setup EF Core DbContext

---
+++ src/onevote/Models/User.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace onevote.Models
+{
+    public class User
+    {
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime BirthDate { get; set; }
+    }
+}
