c6dffcd151fa0a5e29829d7b041ea1c734c5c55a
Will-Frey <wef9308@rit.edu>
2018-04-03 20:56:00 -0400

    Project restructure

--- src/onevote/Startup.cs
+++ src/onevote/Startup.cs
@@ -1,15 +1,17 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using onevote.Data;
 using onevote.Models;
-using onevote.Repositories;
+using onevote.Services;
 
 namespace onevote
 {
@@ -26,11 +28,52 @@ namespace onevote
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<OnevoteContext>(options =>
-                              options.UseNpgsql(Configuration.GetConnectionString("onevoteDatabase")));
+                             options.UseNpgsql(Configuration.GetConnectionString("onevoteDatabase")));
             // services.AddTransient<IBallotRepository, BallotRepository>();
             // services.AddTransient<IUserRepository, UserRepository>();
             // services.AddTransient<ICandidateRepository, CandidateRepository>();
             // services.AddTransient<IElectionRepository, ElectionRepository>();
+
+            services.AddIdentity<ApplicationUser, IdentityRole>()
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddDefaultTokenProviders();
+
+            services.Configure<IdentityOptions>(options =>
+            {
+                // Password settings
+                options.Password.RequireDigit = true;
+                options.Password.RequiredLength = 8;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireUppercase = true;
+                options.Password.RequireLowercase = false;
+                options.Password.RequiredUniqueChars = 6;
+
+                // Lockout settings
+                options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(30);
+                options.Lockout.MaxFailedAccessAttempts = 10;
+                options.Lockout.AllowedForNewUsers = true;
+
+                // User settings
+                options.User.RequireUniqueEmail = true;
+            });
+
+            services.ConfigureApplicationCookie(options =>
+            {
+                // Cookie settings
+                options.Cookie.HttpOnly = true;
+                options.ExpireTimeSpan = TimeSpan.FromMinutes(30);
+                // If the LoginPath isn't set, ASP.NET Core defaults 
+                // the path to /Account/Login.
+                options.LoginPath = "/Account/Login";
+                // If the AccessDeniedPath isn't set, ASP.NET Core defaults 
+                // the path to /Account/AccessDenied.
+                options.AccessDeniedPath = "/Account/AccessDenied";
+                options.SlidingExpiration = true;
+            });
+
+            // Add application services.
+            services.AddTransient<IEmailSender, EmailSender>();
+
             services.AddMvc();
             services.Configure<MvcOptions>(options =>
             {
@@ -53,6 +96,8 @@ namespace onevote
             app.UseStaticFiles();
 
             app.UseMvc();
+
+            app.UseAuthentication();
         }
     }
 }
